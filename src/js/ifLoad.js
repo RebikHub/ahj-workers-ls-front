cfc9be4800b16fba39e66821130c778d79387322
RebikHub <rebikovlk@mai.ru>
2022-01-16 13:46:54 +0700

    start code

--- src/js/ifLoad.js
+++ src/js/ifLoad.js
@@ -1,5 +1,19 @@
 export default class Loading {
     constructor(server) {
         this.server = server;
+        this.newsList = document.querySelector('.')
+        this.news = {
+            date: 'date',
+            img: 'http://url.com',
+            text: 'Dune - 2021'
+        }
+    }
+
+    events() {
+        this.createNews(this.news);
+    }
+
+    createNews(news) {
+        const div
     }
 }
