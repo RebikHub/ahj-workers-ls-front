6cb11ea8d6835bcf62f1847b3d3486b061f8e158
RebiHub <rebikovlk@mail.ru>
2022-01-17 22:29:39 +0700

    re-build

--- webpack.common.js
+++ webpack.common.js
@@ -12,12 +12,6 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.worker\.js$/,
-        use: {
-          loader: 'worker-loader',
-        },
-      },
-      {
         test: /\.js$/,
         exclude: /node_modules/,
         use: {
