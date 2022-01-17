d5b17bb74c9910aa7c7d139096eb63ecbe6d5da8
Alexandr Rebikov <rebikovlk@mail.ru>
2022-01-17 16:02:35 +0700

    webpack

--- webpack.common.js
+++ webpack.common.js
@@ -12,7 +12,7 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.web-worker\.js$/,
+        test: /\.worker\.js$/,
         use: {
           loader: 'worker-loader',
         },
